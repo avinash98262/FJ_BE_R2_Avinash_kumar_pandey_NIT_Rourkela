50e73ec847c74adf19912b2ed8e9d2c8db8f667b
avinash9826 <pandeyavinash960@gmail.com>
2023-09-14 01:02:06 +0530

    maintaining login page

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,8 @@ from .views import *
 
 urlpatterns = [
     path('', home , name="home"),
-    path('login_page/', login_page , name="login_page"),
-    path('register_page/', register_page , name="register_page"),
+    path('accounts/login/', login_page, name="login_page"),
+    path('accounts/register/', register_page, name="register_page"),
+
    
 ]
