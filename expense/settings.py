8ee75f71081553d187382a25fdb9937651d6c510
avinash9826 <pandeyavinash960@gmail.com>
2023-09-14 11:37:16 +0530

    now

--- expense/settings.py
+++ expense/settings.py
@@ -117,18 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'public/static'),
 ]
 
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
